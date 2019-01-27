1099344c29d4909cbd1a2ebadeab0c72e148fc97
Louis Wong <louiswong@Louiss-MacBook-Pro.local>
2019-01-27 17:59:07 -0500

    Added /Shared/Content.js to show Loading text  when coinList().data not ready

---
+++ src/Shared/Content.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { AppContext } from '../App/AppProvider';
+
+const Content = ({children}) => {
+  return (
+    <AppContext.Consumer>
+      {({coinList}) => {
+        if(!coinList) return <div>Loading Coins</div>
+
+        return <div>{children}</div>
+      }}
+    </AppContext.Consumer>
+  )
+}
+
+export default Content
