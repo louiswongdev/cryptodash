d35799f2f50d62267817f35d11c64c04e0169d2f
Louis Wong <louiswong@Louiss-MacBook-Pro.local>
2019-01-28 17:23:37 -0500

    Added components relating to CoinGrid in order to display each tile with coin name, header and image

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -2,20 +2,27 @@ import React from 'react';
 import { AppContext } from '../App/AppProvider';
 import styled, { css } from 'styled-components';
 import { SelectableTile } from '../Shared/Tile';
+import CoinTile from './CoinTile';
 
 export const CoinGridStyled = styled.div`
   display: grid;
   grid-template-columns: repeat(5, 1fr);
   grid-gap: 15px;
+  margin-top: 40px;
 `;
+
+// Return only the first 100 coins
+const getCoinsToDisplay = coinList => {
+  return Object.keys(coinList).slice(0, 100);
+};
 
 const CoinGrid = () => {
   return (
     <AppContext.Consumer>
       {({ coinList }) => (
         <CoinGridStyled>
-          {Object.keys(coinList).map(coinKey => (
-            <SelectableTile> {coinKey} </SelectableTile>
+          {getCoinsToDisplay(coinList).map((coinKey,i) => (
+            <CoinTile coinKey={coinKey} key={i} />
           ))}
         </CoinGridStyled>
       )}
